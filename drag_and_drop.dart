673ea47c6a9c486d5d98f4278088f6c6c805a4f6
Aditya Kumar Singh <94990196+anonymouslyfadeditzme@users.noreply.github.com>
2023-01-14 11:30:37 +0530

    Final Solved Drag and Drop Problem

--- drag_and_drop.dart
+++ drag_and_drop.dart
@@ -1,35 +1,34 @@
- List<String> screens=["Zoom","music","chrome","notes"];
-void main(){
-    // pullScreenToFront(2);
-//    deleteScreen(2);
-//    addScreenToFront("Youtube");
-   findScreenatIndex(2);
+List<String> screens = ["Zoom", "music", "chrome", "notes"];
+void main() {
+  pullScreenToFront(2);
+  deleteScreen(2);
+  addScreenToFront("Youtube");
+  findScreenatIndex(2);
 }
 
-void pullScreenToFront(int x){
-    String temp="";
-     for(int i=x;i>0;i--){
-         temp=screens[i];
-          screens[i]=screens[i-1];
-          screens[i-1]=temp;
-     }
-print(screens);
+void pullScreenToFront(int x) {
+  String temp = "";
+  for (int i = x; i > 0; i--) {
+    temp = screens[i];
+    screens[i] = screens[i - 1];
+    screens[i - 1] = temp;
+  }
+  print(screens);
 }
 
-void deleteScreen(int x){
-   screens.removeAt(x);
-   print(screens);
+void deleteScreen(int x) {
+  screens.removeAt(x);
+  print(screens);
 }
 
-void addScreenToFront(String y){
-    screens.add(y);
-  String temp=screens[screens.length-1];
-  screens[screens.length-1]=screens[0];
-  screens[0]=temp;
-    print(screens);
+void addScreenToFront(String y) {
+  screens.add(y);
+  String temp = screens[screens.length - 1];
+  screens[screens.length - 1] = screens[0];
+  screens[0] = temp;
+  print(screens);
 }
 
-void findScreenatIndex(int a){
-print(screens.elementAt(a));
+void findScreenatIndex(int a) {
+  print(screens.elementAt(a));
 }
-
